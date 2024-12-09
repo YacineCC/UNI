9f23c6af4751bfa27db3638c4a66c9439b0a852f
YacineCC <Haouas.Yacine@hotmail.com>
2024-12-09 01:00:06 +0100

    Début d'affichage des clusters (flop pour l'instant)

---
+++ M1/machine_learning/clustering.py
@@ -0,0 +1,138 @@
+import soundfile as sf
+import scipy.io.wavfile as wave
+#import panda as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy 
+import os
+import sys
+import scipy.cluster.vq as vq
+
+os.chdir(os.path.dirname(__file__))
+
+def matrice_entropies():
+    sig, r = sf.read("ONECAT_20200114_152058_174.wav")
+
+    data = sig[::2]
+    #diff = np.diff(data)
+    diff = np.zeros(len(data))
+    for i in range(1, len(data) - 1):
+        diff[i] = data[i+1]-data[i]
+
+
+    matrice = []
+    matrice_resample = []
+    entropies = []
+    entropies_resample = []
+    pas = 512
+    for start in range(0, len(diff)-pas, pas):
+        interval = diff[start : start + pas * 2]
+
+        auto_corr = scipy.signal.correlate(interval, interval)
+        # Symétrie de l'auto-corrélation donc on prend la moitié et garder le 1/20ème après.
+        resamp_auto_corr = auto_corr[len(auto_corr)//2:len(auto_corr)//2 + len(auto_corr)//20]
+
+        z_resample = resamp_auto_corr - min(resamp_auto_corr) + 1e-100 #sys.float_info.epsilon
+        p_resample = z_resample / sum(z_resample)
+        H_resample = scipy.stats.entropy(p_resample) 
+
+        z = auto_corr - min(auto_corr) + 1e-100 #sys.float_info.epsilon
+        p = z / sum(z)
+        H = scipy.stats.entropy(p)
+
+        entropies.append(H)
+        matrice.append(auto_corr)
+
+        entropies_resample.append(H_resample)
+        matrice_resample.append(resamp_auto_corr)
+
+    np.save("matrice.txt", matrice)
+    np.save("entropies.txt", entropies)
+    np.save("matrice_resample.txt", matrice_resample)
+    np.save("entropies_resample.txt", entropies_resample)
+    
+    return matrice, entropies, matrice_resample, entropies_resample
+
+#matrice, entropies, matrice_resample, entropies_resample = matrice_entropies()
+
+
+
+matrice = np.load("matrice.txt.npy")
+entropies = np.load("entropies.txt.npy")
+matrice_resample = np.load("matrice_resample.txt.npy")
+entropies_resample = np.load("entropies_resample.txt.npy")
+
+
+#matrice = np.array(matrice)
+#cumsum_H = np.cumsum(entropies)
+
+entropies = np.array(entropies_resample)
+idx_faible_entropie = np.where(entropies_resample < 4.5)
+idx_forte_entropie = np.where(entropies_resample > 4.5)
+
+matrice = np.array(matrice_resample)
+mode_1 = matrice[idx_faible_entropie]
+mode_2 = matrice[idx_forte_entropie]
+
+print("Mode 1: ", mode_1.shape)
+print("Mode 2: ", mode_2.shape)
+
+
+# Clustering
+whitened = scipy.cluster.vq.whiten(mode_1)
+codebook,distorsion = scipy.cluster.vq.kmeans(whitened,3)
+
+plt.scatter(whitened[:, 0], whitened[:, 1])
+
+plt.scatter(codebook[:, 0], codebook[:, 1], c='r')
+
+plt.show()
+
+labels, _ = scipy.cluster.vq.vq(whitened, codebook)
+
+# Étape 4 : Liste de couleurs
+colors = ['r', 'g', 'b']  # Une couleur par cluster
+
+# Étape 5 : Tracer les points par cluster
+for i, color in enumerate(colors):
+    cluster_points = whitened[labels == i]  # Points du cluster i
+    plt.scatter(cluster_points[:, 0], cluster_points[:, 1], c=color, label=f'Cluster {i+1}')
+
+# Étape 6 : Tracer les centres de clusters avec un symbole distinct
+plt.scatter(codebook[:, 0], codebook[:, 1], c=colors, marker='x', s=100, label='Centres')
+
+
+fig = plt.figure(figsize=(20, 20))
+entropie_faible_plot = fig.add_subplot(221)
+for i in range(10):
+    entropie_faible_plot.plot(mode_1[i], 'r')
+entropie_faible_plot.set_title("Entropie faible")
+
+entropie_forte_plot = fig.add_subplot(222)
+for i in range(10):
+    entropie_forte_plot.plot(mode_2[i], 'b')
+entropie_forte_plot.set_title("Entropie forte")
+
+
+comparaison_plot = fig.add_subplot(223)
+for i in range(10):
+    comparaison_plot.plot(mode_1[i], 'r')
+    comparaison_plot.plot(mode_2[i], 'b')
+
+comparaison_plot.set_title("Comparaison")
+
+
+
+hist = fig.add_subplot(224)
+
+hist.hist(entropies_resample, 1000, color='orange')
+hist.set_title("Histogramme des entropies (resample)")
+hist.set_xlabel("Entropie")
+hist.set_ylabel("Fréquence")
+
+
+plt.savefig("signal_1sur2_resample.png")
+
+plt.show()
+
+
---
+++ M1/machine_learning/test_autocorr.py
@@ -0,0 +1,55 @@
+import soundfile as sf
+import scipy.io.wavfile as wave
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy 
+import os
+import sys
+
+
+sig, r = sf.read("ONECAT_20200114_152058_174.wav")
+
+indice1 = r * 56
+indice2 = r * 57
+data1 = sig[indice1:indice2]
+
+
+diff = np.zeros(len(data1))
+for i in range(1, len(data1) - 1):
+    diff[i] = data1[i+1]-data1[i]
+
+peak = np.argmax(diff)
+peak_vector_1 = diff[peak-500:peak+500]
+TK_corr_1 = scipy.signal.correlate(peak_vector_1, peak_vector_1)
+peak_vector_2 = diff[peak-200:peak+200]
+TK_corr_2 = scipy.signal.correlate(peak_vector_2, peak_vector_2)
+corr_2_resample = scipy.signal.resample(TK_corr_2, len(TK_corr_2)//10)
+TK_corr = scipy.signal.correlate(diff, diff)
+
+fig = plt.figure(figsize=(20, 20))
+ax1 = fig.add_subplot(321)
+ax1.set_title("Signal")
+ax1.plot(data1)
+
+ax2 = fig.add_subplot(322)
+ax2.plot(diff*5000)
+ax2.set_title("Diff (x5000) seconde 56 à 57")
+
+ax3 = fig.add_subplot(323)
+ax3.plot(TK_corr_1)
+ax3.set_title("Auto_Correlation décalage +-500 seconde 56 à 57")
+
+ax4 = fig.add_subplot(324)
+ax4.plot(TK_corr_2)
+ax4.set_title("Auto_Correlation décalage +-200 seconde 56 à 57")
+
+ax5 = fig.add_subplot(325)
+ax5.plot(corr_2_resample)
+ax5.set_title("Auto_Correlation décalage +-200 resample seconde 56 à 57")
+
+ax6 = fig.add_subplot(326)
+ax6.plot(TK_corr)
+ax6.set_title("Auto_Correlation sur tout le signal")
+
+
+plt.show()
