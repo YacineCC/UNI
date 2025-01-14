7e871cdb540d123dda8ad36caa28007300dfcbd9
Yacine <Haouas.Yacine@hotmail.com>
2025-01-14 14:59:10 +0100

    tp persistence

---
+++ M1/java/bdd/tp01/src/main/java/com/example/jpa/tps/App.java
@@ -0,0 +1,26 @@
+package com.example.jpa.tps;
+import com.example.jpa.tps.person.Person;
+import com.example.jpa.tps.utils.JpaUtil;
+import jakarta.persistence.EntityManager;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        EntityManager em = JpaUtil.getEntityManagerFactory().createEntityManager();
+
+        // Persistance d'une entité
+        em.getTransaction().begin();
+        Person person = new Person("Alice", 25);
+        em.persist(person);
+        em.getTransaction().commit();
+
+        // Lecture de l'entité persistée
+        Person foundPerson = em.find(Person.class, person.getId());
+        System.out.println("Person found: " + foundPerson.getName() + ", Age: " + foundPerson.getAge());
+
+        em.close();
+        JpaUtil.close();
+    }
+}
