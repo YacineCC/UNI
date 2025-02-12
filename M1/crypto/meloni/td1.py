2f54953be1efa9791529ef93b2e1fb0f40944be2
HAOUAS YACINE <88327368+YacineCC@users.noreply.github.com>
2025-02-12 11:38:25 +0100

    fin dechiffrement substitution

--- M1/crypto/meloni/td1.py
+++ M1/crypto/meloni/td1.py
@@ -1,4 +1,5 @@
 import numpy as np
+from math import gcd
 
 def chiffrer(m, k):
     
@@ -19,24 +20,35 @@ freq_french = np.array([7.636, 0.901, 3.260, 3.669, 14.715, 1.066, 0.866, 0.737,
 def analyse_freq(m):
     m = m.lower()
     occurences = np.zeros(26)
+    occurences_doubles = {}
 
-    for l in m :
+    for i, l in enumerate(m) :
         occurences[ord(l) - ord('a')]+= 1
+        if ((m[i], m[i-1]) not in occurences_doubles.keys()):
+
+            occurences_doubles[(m[i], m[i-1])] = 1
+        else :
+            occurences_doubles[(m[i], m[i-1])] += 1
+
+
+    for k in occurences_doubles:
+        occurences_doubles[k] /= len(occurences_doubles)
     
-    return occurences / len(m)
+    
+    return occurences / len(m), dict(sorted(occurences_doubles.items(), key=lambda item : item[1], reverse= True)) 
 
 
 
 def coincidence(m) :
     
     #return sum((occurences * (occurences - 1)) / (len(m) * (len(m) - 1)))
-    freq_text = analyse_freq(m)
+    freq_text = analyse_freq(m)[0]
     return sum(freq_french * freq_text) 
     
 
 
-print(coincidence('cryptographie'))
-alphabet = [chr(i) for i in range(65, 91)]
+#print(coincidence('cryptographie'))
+#alphabet = [chr(i) for i in range(65, 91)]
 #print(alphabet)
 
 
@@ -45,47 +57,116 @@ cipher_ex2 = 'AYAOSXNGWOSXORZYAFIHVYHOSXTIXGSKYFSVIGTFNSTYAOBWXYEYTNIOTGWWXOHOTV
 
 cipher_ex1 = 'PELCGNANYLFRPYNFFVDHR'
 
-ics = []
-for i in range(26):
-    tmp = chiffrer(cipher_ex1, i)
-    ic = coincidence(tmp)
-    ics.append([ic, i])
+#ics = []
+#for i in range(26):
+#    tmp = chiffrer(cipher_ex1, i)
+#    ic = coincidence(tmp)
+#    ics.append([ic, i])
 
-print(sorted(ics, reverse=True)[:5])
-print(chiffrer(cipher_ex1, max(ics)[1]))
+#print(sorted(ics, reverse=True)[:5])
+#print(chiffrer(cipher_ex1, max(ics)[1]))
 
 
 alphabet_dict = {chr(i): chr(i) for i in range(ord('a'), ord('z') + 1)}
 def dsubstitution(m):
-    #alphabet_dict = {chr(i): chr(i) for i in range(ord('a'), ord('z') + 1)}
+    M = m
     alphabet_dict_rev = {}
     m = m.lower()
-    freq = analyse_freq(m)
+    frequences = analyse_freq(m)
+
+    freq = frequences[0]
+
+    freq_double = frequences[1]
 
     idx_text_dec = np.argsort(freq)[::-1]
     idx_french_dec = np.argsort(freq_french)[::-1]
 
    
-    for i in range(26):
-        alphabet_dict_rev[chr(idx_text_dec[i] + 97)] = chr(idx_french_dec[i] + 97)
+    #for i in range(26):
+        #alphabet_dict_rev[chr(idx_text_dec[i] + 97)] = chr(idx_french_dec[i] + 97)
+
+
+    alphabet_dict_rev['y'] = 'e'
+    alphabet_dict_rev['o'] = 'o'
+    alphabet_dict_rev['s'] = 'u'
+    alphabet_dict_rev['n'] = 's'
+    alphabet_dict_rev['i'] = 'i'
+    alphabet_dict_rev['h'] = 'f'
+    alphabet_dict_rev['v'] = 'd'
+    alphabet_dict_rev['t'] = 'n'
+    alphabet_dict_rev['k'] = 'x'
+    alphabet_dict_rev['b'] = 'm'
+    alphabet_dict_rev['e'] = 'h'
+    alphabet_dict_rev['d'] = 'p'
+    alphabet_dict_rev['l'] = 'l'
+    alphabet_dict_rev['m'] = 'g'
+
+    alphabet_dict_rev['f'] = 't'
+    #alphabet_dict_rev['n'] = tmp
+
+    #alphabet_dict_rev['v'] = 't'
+
+    alphabet_dict_rev['a'] = 'c'
+    alphabet_dict_rev['x'] = 'r'
+    alphabet_dict_rev['g'] = 'a'
+    alphabet_dict_rev['w'] = 'p'
+    alphabet_dict_rev['r'] = 'b'
+    alphabet_dict_rev['z'] = 'j'
 
 
 
-    tmp = alphabet_dict_rev['f']
-    alphabet_dict_rev['f'] = alphabet_dict_rev['n']
-    alphabet_dict_rev['n'] = tmp
-
-    tmp = alphabet_dict_rev['i']
-    alphabet_dict_rev['i'] = alphabet_dict_rev['n']
-    alphabet_dict_rev['n'] = tmp
+    
     print(alphabet_dict_rev)
 
     d = ''
-    for l in m:
-        d += alphabet_dict_rev[l]
+    for i, l in enumerate(m):
+        if l in alphabet_dict_rev:
+            d += alphabet_dict_rev[l]
+        else :
+            d += M[i]
 
-
+    print(freq_double, freq)
     return d, coincidence(d)
 
 
-print(dsubstitution(cipher_ex2)) 
+#print(dsubstitution(cipher_ex2)) 
+
+#print(analyse_freq(cipher_ex2))
+
+cipher_ex3 = 'NPGJTJHCIFPZKRIPPVGPWVFRUJWVPQGOIESMHQVGXIPETCCWWMWVTXWEMQYTQWCCWTJLFGEMSFIFLRUWISFINFRGXIOPPGEXTPHWXIUDEIPGNLMTAIWEWWTZCYXULTQDMVTSPOEPDGGWYKNMGEVGCIOAPCNIGAETOIUWIVEVGDHKQJGCIPEIUNSPEVCTVGXIPEEWYWADXGXIFPGJTJHCIOPRVXSPZENALCMIVTUWPGQXQGWIESMHQVGOIEPWCCUWTPWEMNTWGNIRPRFLRVNSOXIEZQRZWCYXEPXVPQGELQOITPWKDXGLMPDMCWEPLPADIFPJTPUWPREPWEPUWTIUEYPLZCYXCRIFPGKDMHDYTWIUNLKQJTPQGYXUXSPZENALCMIVTUWPWEPTGYHCYXNPGJTJHCIFPZKRIPPVGLIVPTGCGGAETWIOLNQCTTFWUTIPQVKPHTTGJVEUTWMTUWTERFFNTIULQGELQOIGYHGAYKDGGEXGPTQBYGTPPZJHCIRWYULYEFRGDIEFVKEI'
+tab = []
+for i in range(len(cipher_ex3)):
+    
+    patern = cipher_ex3[i:i + 4]
+    idx = cipher_ex3.find(patern,i+4)
+    if idx != -1:
+        print(idx - i, patern)
+        if idx - i not in tab :
+            tab.append(idx - i)
+
+pgcd = gcd(tab[0], tab[1])
+for i in range(2, len(tab)):
+    pgcd = gcd(pgcd, tab[i])
+
+
+
+decal_0 = cipher_ex3[::3]
+decal_1 = cipher_ex3[1::3]
+decal_2 = cipher_ex3[2::3]
+analyse_0 = analyse_freq(decal_0)[0]
+cesar_0 = np.argmax(analyse_0)
+
+analyse_1 = analyse_freq(decal_1)[0]
+cesar_1 = np.argmax(analyse_1)
+
+analyse_2 = analyse_freq(decal_2)[0]
+cesar_2 = np.argmax(analyse_2)
+
+decal_0 = chiffrer(decal_0, (cesar_0 - 4) % 26)
+decal_1 = chiffrer(decal_1, (cesar_1 - 4) % 26)
+decal_2 = chiffrer(decal_2, (cesar_2 - 4) % 26)
+
+dechiffer = ''
+
+for i in range(len(decal_0)):
+    dechiffer += []
